3343db78e6a9e5e360cccd7be9918d2f48b8d773
jamalping <jamalping@shenji.local>
2015-05-15 23:57:07 +0800

    Initial Commit

---
+++ CustomTableViewEditDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTableViewEditDemo
+//
+//  Created by jamalping on 15/5/15.
+//  Copyright (c) 2015年 李小平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
